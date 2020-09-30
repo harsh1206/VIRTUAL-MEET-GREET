b4fc2317b3f5a0e9f9c553d3091a501390f8525b
harsh1206 <harshprakash1206@gmail.com>
2020-10-01 02:09:09 +0530

    add login page and its styles

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const expressLayouts = require('express-ejs-layouts');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-
+// using static files
+app.use(express.static("assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
@@ -19,6 +19,8 @@ app.set("layout extractScripts", true);
 
 
 
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(PORT, function (err) {
   
